cb1a76a28027e39937b27249a602a32015cda46d
Alerjop3d <alerjop3d@gmail.com>
2024-11-10 14:30:21 -0300

    corrections applied

--- YasmedicalDB.sql
+++ YasmedicalDB.sql
@@ -5,21 +5,23 @@ CREATE TABLE `Proveedor` (
   `rut` varchar(12) unique,
   `razon_social` varchar(60) NOT NULL,
   `direccion` varchar(100) NOT NULL,
-  `telefono` varchar(11) NOT NULL,
+  `telefono` varchar(14) NOT NULL,
   `email` varchar(60) NOT NULL,
   PRIMARY KEY (`rut`)
 );
 
 CREATE TABLE `suministra` (
+   `id_suministra` INT AUTO_INCREMENT,
   `rut_proveedor` varchar(12) NOT NULL,
-  `fecha_suministro` date NOT NULL
+  `fecha_suministro` date NOT NULL,
+   `codigo` varchar(15) NOT NULL,
+   PRIMARY KEY (id_suministra)
 );
 
 CREATE TABLE `Productos` (
   `codigo` varchar(15) unique,
-  `rut_proveedor` varchar(12) NOT NULL,
   `descripcion` varchar(100) NOT NULL,
-  `cantidad` varchar(11) NOT NULL,
+  `stock` INT NOT NULL,
   `valor` decimal(10,2) NOT NULL,
   PRIMARY KEY (`codigo`)
 );
@@ -27,8 +29,7 @@ CREATE TABLE `Productos` (
 CREATE TABLE `Vendedores` (
   `rut` varchar(12) unique,
   `nombre` varchar(20) NOT NULL,
-  `apellido` varchar(20) NOT NULL,
-  `n°_factura` int NOT NULL,
+  `apellido` varchar(20) NOT NULL,  
   PRIMARY KEY (`rut`)
 );
 
@@ -37,7 +38,8 @@ CREATE TABLE `Facturas` (
   `rut_cliente` varchar(12) NOT NULL,
   `cod_producto` varchar(15) NOT NULL ,
   `valor_producto` decimal(10, 2) NOT NULL,
-  `iva` decimal(10, 2 ) default 0.19,
+  `rut` varchar(12) NOT NULL,
+   cantidad INT NOT NULL,
   PRIMARY KEY (`n°_factura`)
 );
 
@@ -45,17 +47,16 @@ CREATE TABLE `Clientes` (
   `rut` varchar(12) unique,
   `nombre` varchar(20) NOT NULL,
   `apellido` varchar(20) NOT NULL,
-  `telefono` varchar(11) NOT NULL,
+  `telefono` varchar(14) NOT NULL,
   `email` varchar(60) NOT NULL,
   PRIMARY KEY (`rut`)
 );
 
 ALTER TABLE Suministra ADD CONSTRAINT rut_prov FOREIGN KEY (rut_proveedor) REFERENCES Proveedor(rut);
-ALTER TABLE Productos ADD CONSTRAINT IDNumber_prov FOREIGN KEY (rut_proveedor) REFERENCES Proveedor(rut);
-ALTER TABLE Vendedores ADD CONSTRAINT factura_N° FOREIGN KEY (n°_factura) REFERENCES Facturas(n°_factura);
+ALTER TABLE Suministra ADD CONSTRAINT cod_prod FOREIGN KEY (codigo) REFERENCES Productos(codigo);
+ALTER TABLE Facturas ADD CONSTRAINT rut_vendedor FOREIGN KEY (rut) REFERENCES Vendedores(rut);
 ALTER TABLE Facturas ADD CONSTRAINT client_rut FOREIGN KEY (rut_cliente) REFERENCES Clientes(rut);
 ALTER TABLE Facturas ADD CONSTRAINT product_ID FOREIGN KEY (cod_producto) REFERENCES Productos(codigo);
-
 
 
 
